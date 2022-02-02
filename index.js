cd07cfa0258497777f11f0b34a1b702bdae0cec9
KariMokni <rstarokt@gmail.com>
2022-02-02 15:19:34 -0500

    commit

---
+++ index.js
@@ -0,0 +1,38 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then(function (reg) {
+      console.log("Scope" + reg.scope);
+    })
+    .catch(function (error) {
+      console.log("Error" + error);
+    });
+}
+
+// Initialise la variable
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // pas de barre si installé
+  e.preventDefault();
+  // Réserve evenement
+  deferredPrompt = e;
+  // installer pwa fenêtre avertissement
+  installApp();
+
+  console.log(`'beforeinstallprompt' fired.`);
+
+  // bouton installer
+  appButton.addEventListener("click", function () {
+    deferredPrompt.prompt();
+  });
+});
+
+// informer l'utilisateur de la possibilité d'installer pwa
+
+function installApp() {
+  console.log("fonctionne");
+  const fenetre = document.getElementById("toast");
+  const toast = new bootstrap.Toast(fenetre, { delay: 5000 }); //affichée 5 secondes
+  toast.show();
+}
---
+++ sw.js
@@ -0,0 +1,47 @@
+this.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installation de v1");
+  event.waitUntil(
+    caches.open("v1").then(function (cache) {
+      console.log("[Service Worker] Mise en cache");
+      return cache.addAll([
+        "index.html",
+        "index.js",
+        "assets/icon-192x192.png",
+        "assets/icon-256x256.png",
+        "assets/icon-384x384.png",
+        "assets/icon-512x512.png",
+        "bootstrap-5.1.3-dist/css/bootstrap.min.css",
+        "icons-1.7.2/font/bootstrap-icons.css",
+        "bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js",
+        "img/architecture.jpg",
+        "img/djerbahood.jpg",
+        "img/djerbalogo.jpg",
+        "img/plage.jpg",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          let responseClone = response.clone();
+
+          caches.open("v1").then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+
+          return response;
+        });
+      }
+    })
+  );
+});
+
+this.addEventListener("activate", (e) => {
+  console.log("sw actif");
+});
